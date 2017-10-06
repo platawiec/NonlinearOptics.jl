c496c21a381e301fdd45186d4f7f7ef850f1abe4
platawiec <platawiec@hotmail.com>
2017-10-06 13:27:51 -0400

    Lugatio Lefever and new plot recipe

--- src/plot_recipes.jl
+++ src/plot_recipes.jl
@@ -1,8 +1,21 @@
 @recipe function f(sol::NLSESolution;plot_analytic=false)
     if sol.tslocation == 0
+        layout := @layout [intime
+                           inspectrum]
         @series begin
+            subplot := 1
             seriestype := :path
             sol.prob.zmesh, abs2.(sol.u[end])
+        end
+        dω = 2pi/maximum(prob.zmesh)
+        ω_max = dktilde/2.0*(length(prob.zmesh)-1)
+        ω = collect(-ktilde_max:dktilde:ktilde_max)
+        spectrum = abs2.(fftshift(fft(sol.u[end])))
+        spectrum_dB = 10*log10.(spectrum/maximum(spectrum))
+        @series begin
+            subplot := 2
+            seriestype := :path
+            ω, spectrum_dB
         end
     else
         out = Any[]
