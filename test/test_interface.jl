7f9515fa85be21179b27ab1f8dc3b028a6d0c3b5
platawiec <platawiec@hotmail.com>
2017-10-12 17:46:48 -0400

    More tests

--- test/test_interface.jl
+++ test/test_interface.jl
@@ -10,11 +10,3 @@ attr = OpticalAttr(1.0, "Attribute")
 @test NonlinearOptics.der(attr, 100e-9) == 0.0
 @test NonlinearOptics.der(attr, 100e-9; order=0) == 1.0
 @test NonlinearOptics.der(attr, 1.0; order=3) == 0.0
-
-f = Frequency.(collect(linspace(c/1000e-9, c/500e-9, 20)))
-neff = collect(linspace(1, 2, 20).^2)
-attr = OpticalAttr(f, neff, "neff")
-@test attr(f[10]) ≈ neff[10] # fit function should be close to data
-@test NonlinearOptics.der(attr, getω(f[end]); order=0) ≈ 4.0 #derivative at 0 order should
-                                             #should return neff
-@test get_groupindex(attr, f[1]) ≈ 3.0 # =neff + 2*neff in this special case
