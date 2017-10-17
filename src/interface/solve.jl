8ee6a996ba8cf8c105b7996af11ea5e659e33b7c
platawiec <platawiec@hotmail.com>
2017-10-17 13:13:31 -0400

    Model problem building

--- src/interface/solve.jl
+++ src/interface/solve.jl
@@ -8,7 +8,8 @@ end
     build_problem()
 Sets up a Lugatio-Lefever problem
 """
-function build_problem(laser::CWLaser, res::AbstractResonator; dispersion_order=2)
+function build_problem(laser::CWLaser, res::AbstractResonator;
+                       dispersion_order=2, additional_terms=[])
     # TODO: support mode coupling
     # TODO: currently only supports one mode
     mode = res.modes[1]
@@ -30,6 +31,54 @@ function build_problem(laser::CWLaser, res::AbstractResonator; dispersion_order=
     prob_LL = NLSEProblem(N_LL, D_LL, u0_LL, τspan, tmesh)
 end
 
-function build_problem(laser::PulsedLaser, wg::Waveguide; dispersion_order=2)
-    prob_NLSE = NLSEProblem()
+function build_problem(laser::PulsedLaser, wg::Waveguide;
+                       dispersion_order=2, additional_terms=[])
+    # TODO: support mode coupling
+    # TODO: currently only supports one mode
+    wg = wg.modes[1]
+
+    α = mode.linearloss
+    γnl = get_nonlinearcoeff(res, mode, laser.frequency)
+    L = wg.length
+
+    beta = get_beta(mode, laser.frequency, dispersion_order)
+    beta_coeff = beta ./ [factorial(n) for n=0:(length(beta)-1)]
+    tmesh = linspace(-0.5, 0.5, 2^10)
+
+    u0 = derive_pulse(laser)
+    self_steepening(z, u) = 0.0
+    raman_response(z, u) = 0.0
+    if :self_steepening in additional_terms
+        ω0 = getω(laser.frequency)
+        self_steepening(z, u) = - γnl/ω0 * diff_cyclic(abs2(u)) / dt * u
+    end
+    if :raman_response in additional_terms
+        raman_response(z, u) - 1im*γnl*raman_response(wg.material)
+    end
+
+    function N(z, u)
+        (-α/2 + 1im * γnl * abs2(u)) * u + self_steepening(z, u) + raman_response(z, u)
+    end
+    function D(ω, u)
+        1im * Poly(beta_coeff, :ω)
+    end
+    prob = NLSEProblem(N, D, u0, zspan, tmesh)
+end
+
+function diff_cyclic(A)
+    B = similar(A)
+    @inbounds B[1] = A[1]-A[end]
+    for i=2:length(B)
+        @inbounds B[i] = A[i]-A[i-1]
+    end
+    B
+end
+
+function diff_cyclic!(A)
+    @inbounds tmp = A[1]-A[end]
+    for i=1:(length(A)-1)
+        @inbounds A[i] = A[i+1]-A[i]
+    end
+    @inbounds A[end] = tmp
+    A
 end
